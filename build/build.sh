fbc2de564e7bd39d0ca21b32c4b193159ecab6c4
Shukri Adams <shukri.adams@gmail.com>
2021-02-01 17:21:40 +0100

    added allruntimes container

--- build/build.sh
+++ build/build.sh
@@ -1,3 +1,6 @@
+# fail on all errors
+set -e
+
 # when running on a CI system like travis, use switch "--ci"
 # when running locally, force clean jspm build with "--jspm"
 CI=0
@@ -15,7 +18,7 @@ done
 # We use cp on CI systems because we can't be sure rsync is available there
 # NOTE : jspm hits github each time you run a local build. If you do this enough times you'll hit your github rate limit
 
-mkdir -p .clone &&    
+mkdir -p .clone 
     
 if [ $CI -eq 1 ]; then
     # if running on CI system, copy everything from src to .clone folder
@@ -28,38 +31,42 @@ fi
 
 # build step 1: build frontend CSS/JS, and leaves it behind in .clone/src/dist folder. This build will yarn install dev modules, which we
 # want to delete
-docker run -v $(pwd)/.clone:/tmp/build shukriadams/node10build:0.0.3 sh -c "cd /tmp/build/src/build && sh ./build.sh --clean --version" &&
-sudo rm -rf .clone/src/node_modules &&
+docker run -v $(pwd)/.clone:/tmp/build shukriadams/node10build:0.0.3 sh -c "cd /tmp/build/src/build && sh ./build.sh --clean --version" 
+sudo rm -rf .clone/src/node_modules
 
 # build step 2: run a second container install, this one yarn installs "production" modules only
-docker run -v $(pwd)/.clone:/tmp/build shukriadams/node10build:0.0.3 sh -c "cd /tmp/build/src && yarn --no-bin-links --ignore-engines --production" &&
+docker run -v $(pwd)/.clone:/tmp/build shukriadams/node10build:0.0.3 sh -c "cd /tmp/build/src && yarn --no-bin-links --ignore-engines --production" 
 
 # combine artifacts from steps 1 and 2 and zip them
-rm -rf .stage &&
-mkdir -p .stage &&
-mkdir -p .stage/public &&
-cp -R .clone/src/node_modules .stage &&
-cp -R .clone/src/server .stage &&
-cp -R .clone/src/public .stage &&
-cp .clone/src/index.js .stage &&
-cp .clone/src/package.json .stage &&
-rm -rf .stage/server/reference &&
-rm -f node/.stage.tar.gz &&
-tar -czvf node/.stage.tar.gz .stage &&
+rm -rf .stage 
+mkdir -p .stage
+mkdir -p .stage/public 
+cp -R .clone/src/node_modules .stage 
+cp -R .clone/src/server .stage 
+cp -R .clone/src/public .stage
+cp .clone/src/index.js .stage
+cp .clone/src/package.json .stage 
+rm -rf .stage/server/reference 
+rm -f node/.stage.tar.gz 
+tar -czvf node/.stage.tar.gz .stage 
 
-# build 3: Build the final container, using the zip. We do this in a subfolder so we can limit the size of the docker build context,
+# build 3: Build the base container, using the zip. We do this in a subfolder so we can limit the size of the docker build context,
 # else docker will pass in everything in current folder 
-cd node
-docker build -t shukriadams/whobrokethebuild . &&
+cd base
+docker build -t shukriadams/whobrokethebuild .
 cd -
 
+cd allruntimes
+docker build -t shukriadams/whobrokethebuild-allruntimes .
+cd - 
+
 # test build
-docker-compose -f docker-compose-testmount.yml down &&
-docker-compose -f docker-compose-testmount.yml up -d &&
+docker-compose -f docker-compose-testmount.yml down 
+docker-compose -f docker-compose-testmount.yml up -d 
 # give container a chance to start
-sleep 1 &&
+sleep 1 
 # confirm app has started
-LOOKUP=$(curl --silent localhost:4000/isalive) &&
+LOOKUP=$(curl --silent localhost:4000/isalive) 
 if [ "$LOOKUP" != "1" ] ; then
     echo "ERROR : container test failed"
     exit 1
@@ -67,12 +74,13 @@ fi
 
 echo "container test passed"
 
-
 if [ $DOCKERPUSH -eq 1 ]; then
-    TAG=$(git describe --tags --abbrev=0) &&
-    docker login -u $DOCKER_USER -p $DOCKER_PASS &&
-    docker tag shukriadams/whobrokethebuild:latest shukriadams/whobrokethebuild:$TAG &&
+    TAG=$(git describe --tags --abbrev=0) 
+    docker login -u $DOCKER_USER -p $DOCKER_PASS 
+    docker tag shukriadams/whobrokethebuild:latest shukriadams/whobrokethebuild:$TAG 
+    docker tag shukriadams/whobrokethebuild-allruntimes:latest shukriadams/whobrokethebuild-allruntimes:$TAG
     docker push shukriadams/whobrokethebuild:$TAG 
+    docker push shukriadams/whobrokethebuild-allruntimes:$TAG
 fi
 
 echo "Build complete"
