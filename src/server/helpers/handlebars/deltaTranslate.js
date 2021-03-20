9bfcad78ce6dc0777c0c127525b4c25169cd84fe
Shukri Adams <shukri.adams@gmail.com>
2021-03-20 11:37:57 +0100

    fixed delta translate
    now handles builds that don't have processed delta

--- src/server/helpers/handlebars/deltaTranslate.js
+++ src/server/helpers/handlebars/deltaTranslate.js
@@ -1,35 +1,66 @@
 const constants = require(_$+'types/constants')
 
 module.exports = Handlebars => {
+    /**
+     * Translates delta value into user-friendly text. Because delta is not always available but status
+     * is always there, we use status as fallback value.
+     */
+    Handlebars.registerHelper('deltaTranslate', function(delta, status){
 
-    Handlebars.registerHelper('deltaTranslate', function(delta){
-
+        let result = null
         switch(delta){
             case constants.BUILDDELTA_PASS : {
-                return 'Passing'
+                result = 'Passing'
+                break
             }
 
             case constants.BUILDDELTA_FIX : {
-                return 'Fixed build'
+                result = 'Fixed build'
+                break
             }
 
             case constants.BUILDDELTA_CAUSEBREAK : {
-                return 'Broke build'
+                result = 'Broke build'
+                break
             }
 
             case constants.BUILDDELTA_CONTINUEBREAK : {
-                return 'Already broken'
+                result = 'Already broken'
+                break
             }
 
             case constants.BUILDDELTA_CHANGEBREAK : {
-                return 'Changed break'
-            }
-
-            default : {
-                return `Unknown delta ${delta}`
+                result = 'Changed break'
+                break
             }
         }
 
+        // back to status
+        if (!result)
+            switch(status){
+                case constants.BUILDSTATUS_FAILED : {
+                    result = 'Broken'
+                    break
+                }
+
+                case constants.BUILDSTATUS_PASSED : {
+                    result = 'Passing'
+                    break
+                }
+
+                case constants.BUILDSTATUS_INPROGRESS : {
+                    result = 'Building'
+                    break
+                }
+            }
+
+        // this is unusual
+        if (!result){
+            __log.info(`Unhandled delta : "${delta}" and status:`)
+            result = 'Unknown'
+        }
+
+        return result
     })
     
 }
