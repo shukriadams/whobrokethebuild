77bf6329c2bd0589b9ead48f085611fd87e57800
Shukri Adams <shukri.adams@gmail.com>
2023-06-19 19:51:11 +0200

    changed busy text

--- src/Wbtb.Core.Web/Controllers/HomeController.cs
+++ src/Wbtb.Core.Web/Controllers/HomeController.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
 using System.Collections.Generic;
-using System.DirectoryServices.ActiveDirectory;
 using System.Linq;
 using System.Web;
 using Wbtb.Core.Common;
