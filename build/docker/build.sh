67b2514280e4cd081d82ed216e60fbb8d2fa8b00
Shukri Adams <shukri.adams@gmail.com>
2024-11-29 12:21:19 +0100

    build script
    - added switch for legacy compose
    - added nobuild switch

--- build/docker/build.sh
+++ build/docker/build.sh
@@ -1,11 +1,15 @@
 set -e
 DOCKERPUSH=0
 SMOKETEST=0
+COMPOSE="docker compose"
+BUILD=1
 
 while [ -n "$1" ]; do 
     case "$1" in
     --push|-p) DOCKERPUSH=1 ;;
     --test|-t) SMOKETEST=1 ;;
+    --nobuild) BUILD=0 ;;
+    --legacy-compose|-t) COMPOSE="docker-compose" ;;
     esac 
     shift
 done
@@ -20,61 +24,64 @@ fi
 # write hhash + tag to currentVersion.txt in source, this will be displayed by web ui
 echo "$HASH $TAG" > ./../../src/Wbtb.Core.Web/currentVersion.txt 
 
-echo "Building CLI ..."
-docker run \
-    -e TAG=$TAG \
-    -v $(pwd)./../../src:/tmp/wbtb \
-    mcr.microsoft.com/dotnet/sdk:6.0 \
-    sh -c "cd /tmp/wbtb && \
-        dotnet restore Wbtb.Core.CLI && \
-        dotnet publish Wbtb.Core.CLI --configuration Release --runtime linux-x64"
+if [ $BUILD -eq 1 ]; then
 
-echo "Building web server ..."
-docker run \
-    -e TAG=$TAG \
-    -v $(pwd)./../../src:/tmp/wbtb \
-    mcr.microsoft.com/dotnet/sdk:6.0 \
-    sh -c "cd /tmp/wbtb && \
-        dotnet restore Wbtb.Core.Web && \
-        dotnet publish Wbtb.Core.Web /property:PublishWithAspNetCoreTargetManifest=false --configuration Release"
+    echo "Building CLI ..."
+    docker run \
+        -e TAG=$TAG \
+        -v $(pwd)./../../src:/tmp/wbtb \
+        mcr.microsoft.com/dotnet/sdk:6.0 \
+        sh -c "cd /tmp/wbtb && \
+            dotnet restore Wbtb.Core.CLI && \
+            dotnet publish Wbtb.Core.CLI --configuration Release --runtime linux-x64"
 
-echo "Building web static assets ..."
-docker run \
-    -v $(pwd)./../../src:/tmp/wbtb \
-    shukriadams/node12build:0.0.4 \
-    sh -c "cd /tmp/wbtb/Wbtb.Core.Web/frontend && \
-        sh ./setup.sh
-        npm install && \
-        npm run icons && \
-        npm run build"
+    echo "Building web server ..."
+    docker run \
+        -e TAG=$TAG \
+        -v $(pwd)./../../src:/tmp/wbtb \
+        mcr.microsoft.com/dotnet/sdk:6.0 \
+        sh -c "cd /tmp/wbtb && \
+            dotnet restore Wbtb.Core.Web && \
+            dotnet publish Wbtb.Core.Web /property:PublishWithAspNetCoreTargetManifest=false --configuration Release"
 
-echo "Building Message queue ..."
-docker run \
-    -v $(pwd)./../../src:/tmp/wbtb \
-    mcr.microsoft.com/dotnet/sdk:6.0 \
-    sh -c "cd /tmp/wbtb && \
-        dotnet restore MessageQueue && \
-        dotnet publish MessageQueue /property:PublishWithAspNetCoreTargetManifest=false --configuration Release"
+    echo "Building web static assets ..."
+    docker run \
+        -v $(pwd)./../../src:/tmp/wbtb \
+        shukriadams/node12build:0.0.4 \
+        sh -c "cd /tmp/wbtb/Wbtb.Core.Web/frontend && \
+            sh ./setup.sh
+            npm install && \
+            npm run icons && \
+            npm run build"
 
-# build web container
+    echo "Building Message queue ..."
+    docker run \
+        -v $(pwd)./../../src:/tmp/wbtb \
+        mcr.microsoft.com/dotnet/sdk:6.0 \
+        sh -c "cd /tmp/wbtb && \
+            dotnet restore MessageQueue && \
+            dotnet publish MessageQueue /property:PublishWithAspNetCoreTargetManifest=false --configuration Release"
 
-cd ./../../src
+    # build web container
 
-echo "Building cli container ..."
-docker build -t shukriadams/wbtb-cli -f Dockerfile-CLI . 
-docker tag shukriadams/wbtb-cli:latest shukriadams/wbtb-cli:$TAG 
+    cd ./../../src
 
-echo "Building web container ..."
-docker build -t shukriadams/wbtb -f Dockerfile-Web . 
-docker tag shukriadams/wbtb:latest shukriadams/wbtb:$TAG 
-cd -
+    echo "Building cli container ..."
+    docker build -t shukriadams/wbtb-cli -f Dockerfile-CLI . 
+    docker tag shukriadams/wbtb-cli:latest shukriadams/wbtb-cli:$TAG 
+
+    echo "Building web container ..."
+    docker build -t shukriadams/wbtb -f Dockerfile-Web . 
+    docker tag shukriadams/wbtb:latest shukriadams/wbtb:$TAG 
+    cd -
+fi
 
 if [ $SMOKETEST -eq 1 ]; then
-    docker compose -f docker-compose.yml down --remove-orphans
-    docker compose -f docker-compose.yml up -d 
+    $COMPOSE -f docker-compose.yml down --remove-orphans
+    $COMPOSE -f docker-compose.yml up -d 
     sleep 5  # wait a few seconds to make sure app in container has started
     STATUS=$(curl -s -o /dev/null -w "%{http_code}" localhost:49022/error/notready) 
-    docker compose -f docker-compose.yml down 
+    $COMPOSE -f docker-compose.yml down 
     if [ "$STATUS" != "503" ]; then
         echo "test container returned unexpected value ${STATUS}. Container log is:"
         echo $(docker logs wbbt-test)
